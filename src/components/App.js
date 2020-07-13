62be815899838720554535118e91b4c063c289b9
acrucha <mcaa@cin.ufpe.br>
2020-07-13 11:21:28 -0300

    SearchBar and API update

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+import youtube from '../api/youtube';
+
+const KEY = 'AIzaSyDKmGWg060WVuvzqjQGIZx17ZSpMqcjkOw';
+
+class App extends React.Component {
+
+    onTermSubmit = async term => {
+        const response = await youtube.get("/search", {
+          params: {
+            q: term,
+            part: "snippet",
+            maxResults: 5,
+            type: 'video',
+            key: `${KEY}`
+          }
+        });
+
+        console.log(response);
+    }
+
+    render(){
+        return (
+            <div className="ui container">
+                <SearchBar onFormSubmit={this.onTermSubmit}/>
+            </div>
+        ); 
+    }
+
+};
+
+export default App;
